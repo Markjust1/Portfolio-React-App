da29bbd926f34ed80e2bf3b753a09113104f7da3
Markjust1 <novikov711@gmail.com>
2022-09-29 09:12:28 -0600

    add proj pic plus resize

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,16 @@
 import "./Project.css";
 
-
 function Project(props) {
   return (
     <div className="project">
       <div className="name">{props.name}</div>
-      <div className="image"><img src={props.image} alt="" /></div>
-      <div className="description">Description{props.description}</div>
-      <div className="tech">Tech Stack: {props.tech}</div>
+      <div className="proj_desc">
+        <div className="image">
+          <img src={props.image} alt="" />
+        </div>
+        <div className="description">{props.description}</div>
+        <div className="tech">{props.tech}</div>
+      </div>
     </div>
   );
 }
