be8748d5a32bdd0fa2d46c94971d1211c0d79c31
Markjust1 <novikov711@gmail.com>
2022-10-02 23:06:27 -0600

    hello message

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,12 +3,10 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="nav">
-      <h2>Welcome</h2>
       <div className="nav-items">
         <h4>Home</h4>
         <h4>My Projects</h4>
         <h4>Tech Stack</h4>
-        <h4>About Me</h4>
       </div>
     </div>
   );
