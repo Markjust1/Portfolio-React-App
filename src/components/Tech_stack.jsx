853ac43921bcb714a3f141540dd129164d9d3e8e
Markjust1 <novikov711@gmail.com>
2022-10-05 21:56:18 -0600

    add js icon

--- src/components/Tech_stack.jsx
+++ src/components/Tech_stack.jsx
@@ -1,10 +1,14 @@
 import "./Tech_stack.css";
+import js from "../assets/js.svg";
 
 function Tech_stack() {
 
   return (
     <div className="tech_stack">
       <div className="animate-charcter">My Tech Stack:</div>
+      <div className="stack-container">
+        <img src={js}></img>
+      </div>
     </div>
   );
 }
