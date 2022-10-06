ed4e4be1d4cc7eac6917386c1b76af5e2b185f38
Markjust1 <novikov711@gmail.com>
2022-10-05 22:33:36 -0600

    add teck stack to components

--- src/components/Tech_stack.jsx
+++ src/components/Tech_stack.jsx
@@ -11,12 +11,24 @@ import postgresql from "../assets/postgresql.svg";
 import node from "../assets/node.svg";
 
 function Tech_stack() {
-
   return (
     <div className="tech_stack">
       <div className="animate-charcter">My Tech Stack:</div>
       <div className="stack-container">
-        <img src={js}></img>
+        <div className="first-stack">
+          <img src={html}></img>
+          <img src={css}></img>
+          <img src={js}></img>
+          <img src={react}></img>
+          <img src={ruby}></img>
+        </div>
+        <div className="second-stack">
+          <img src={postgresql}></img>
+          <img src={node}></img>
+          <img src={git}></img>
+          <img src={jquery}></img>
+          <img src={bootstrap}></img>
+        </div>
       </div>
     </div>
   );
