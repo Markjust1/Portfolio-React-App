afcb46cc2dd356f1ecb4d666699057e3314483b7
Markjust1 <novikov711@gmail.com>
2022-10-05 21:51:18 -0600

    add tech stack

--- src/components/Tech_stack.jsx
+++ src/components/Tech_stack.jsx
@@ -1,18 +1,10 @@
 import "./Tech_stack.css";
 
 function Tech_stack() {
-  const frontend = ['React', 'Javascript', 'HTML', 'CSS'];
-  const backend = ['Node.js', 'Express.js', 'PostgreSQL', 'Ruby on Rails'];
-  const listStack = () => {
-    frontend.forEach((element) => {
-      return <li>{element}</li>
-    })
-  }
 
   return (
-    <div>
-      <ul>{listStack}</ul>
-      {/* <ul>{<li>listStack</li>}</ul> */}
+    <div className="tech_stack">
+      <div className="animate-charcter">My Tech Stack:</div>
     </div>
   );
 }
