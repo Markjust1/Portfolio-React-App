328532a190811dd6891dace6e6418ac775f6f02f
Markjust1 <novikov711@gmail.com>
2022-10-14 17:54:51 -0600

    ad footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation";
 import TechStack from "./components/TechStack";
 import ProjectList from "./components/ProjectList";
 import About from "./components/About";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       </div>
       <TechStack />
       <ProjectList />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="menu">
+      <div className="label">Follow Me</div>
+      <div className="spacer"></div>
+      <div className="item-container">
+        <div className="item">
+          <span>LinkedIn</span>
+        </div>
+        <div className="item">
+          <span>Instagram</span>
+        </div>
+        <div className="item">
+          <span>Facebook</span>
+        </div>
+        <div className="item">
+          <span>Medium</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
