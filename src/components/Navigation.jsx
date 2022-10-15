b206ae0076edaeed123ecacf3f061ed6a64cebb1
Markjust1 <novikov711@gmail.com>
2022-10-14 18:54:44 -0600

    redo navbar, add anim

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,13 +5,30 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <div className="nav">
-      <div className="nav-items">
-        <img src={home} alt=''></img>
-        <img src={tech} alt=''></img>
-        <img src={projects} alt=''></img>
-      </div>
-    </div>
+    // <div className="nav">
+    //   <div className="nav-items">
+    //     <img src={home} alt=''></img>
+    //     <img src={tech} alt=''></img>
+    //     <img src={projects} alt=''></img>
+    //   </div>
+    // </div>
+<div>
+
+    <div className="header"></div>
+  <input type="checkbox" className="openSidebarMenu" id="openSidebarMenu"/>
+  <label for="openSidebarMenu" className="sidebarIconToggle">
+    <div className="spinner diagonal part-1"></div>
+    <div className="spinner horizontal"></div>
+    <div className="spinner diagonal part-2"></div>
+  </label>
+  <div id="sidebarMenu">
+    <ul className="sidebarMenuInner">
+      <li><img src={home} alt="" /></li>
+      <li><img src={projects} alt="" /></li>
+      <li><img src={tech} alt="" /></li>
+    </ul>
+  </div>
+</div>
   );
 }
 
