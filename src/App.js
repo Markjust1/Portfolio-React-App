b9f0e4965e24621fcc33ba3692cb4d4aad31f04f
Markjust1 <novikov711@gmail.com>
2022-10-16 21:05:33 -0600

    add scrolling animation when pressing nav buttons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <div className="curved">
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,34 +1,35 @@
+import * as Scroll from 'react-scroll';
 import home from "../assets/home.png";
 import projects from "../assets/projects.png";
 import tech from "../assets/tech.png";
 import "./Navigation.css";
 
 function Navigation() {
+  var Scroll = require('react-scroll');
+  var scroll = Scroll.animateScroll;
   return (
-    // <div className="nav">
-    //   <div className="nav-items">
-    //     <img src={home} alt=''></img>
-    //     <img src={tech} alt=''></img>
-    //     <img src={projects} alt=''></img>
-    //   </div>
-    // </div>
-<div>
-
-    <div className="header"></div>
-  <input type="checkbox" className="openSidebarMenu" id="openSidebarMenu"/>
-  <label for="openSidebarMenu" className="sidebarIconToggle">
-    <div className="spinner diagonal part-1"></div>
-    <div className="spinner horizontal"></div>
-    <div className="spinner diagonal part-2"></div>
-  </label>
-  <div id="sidebarMenu">
-    <ul className="sidebarMenuInner">
-      <li><img src={home} alt="" /></li>
-      <li><img src={projects} alt="" /></li>
-      <li><img src={tech} alt="" /></li>
-    </ul>
-  </div>
-</div>
+    <div>
+      <div className="header"></div>
+      <input type="checkbox" className="openSidebarMenu" id="openSidebarMenu" />
+      <label htmlFor="openSidebarMenu" className="sidebarIconToggle">
+        <div className="spinner diagonal part-1"></div>
+        <div className="spinner horizontal"></div>
+        <div className="spinner diagonal part-2"></div>
+      </label>
+      <div id="sidebarMenu">
+        <ul className="sidebarMenuInner">
+          <li>
+            <img src={home} alt="" onClick={() => { scroll.scrollTo(0)}} />
+          </li>
+          <li>
+            <img src={tech} alt="" onClick={() => { scroll.scrollTo(1150)}}/>
+          </li>
+          <li>
+            <img src={projects} alt="" onClick={() => { scroll.scrollTo(2100)}}/>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
